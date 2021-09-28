5145551d0b16ce52464021f74dc3ab7a06be4a4b
darya <dapishta@gmail.com>
2021-09-28 12:55:53 +0800

    Внесла правки, а также ответила на те комментарии, где не совсем поняла

--- js/main.js
+++ js/main.js
@@ -1,50 +1,24 @@
-function getRandomNumber (min, max) {
-  if (max < min) {
-      return 'Please choose a range where min is less than max';
-  } else if (min < 0) {
-      return 'Please choose a range where min is equal or higher than 0';
-  } else if ( max === min) {
-      return 'Please choose not equal min and max numbers';
-  }
- return Math.random() * (max - min) + min;
-}
+function getRandomNumber (min, max, numberOfSymbolsAfterComma) {
 
-
-function getRandomIntegralNumber (min, max) {
-
-  let randomNumber = getRandomNumber(min,max); 
-
-  if (typeof randomNumber  === 'number') {
-    return Math.floor(randomNumber);
+  if (min < 0) {
+    // eslint-disable-next-line no-alert
+    alert('Please choose a range where min is equal or higher than 0');
+    return NaN;
+  } else if (max < min) {
+    const swap = min;
+    min = max;
+    max = swap;
+  } else if (numberOfSymbolsAfterComma < 0) {
+    // eslint-disable-next-line no-alert
+    alert('Please choose the number of symbols after comma greater than zero');
+    return NaN;
   }
 
-  return randomNumber;
+  const randomNumber = Math.random() * (max - min) + min;
 
-};
-
-
-function getRandomFractionalNumber (min, max, numberOfSymbolsAfterComma) {
-
-  if (numberOfSymbolsAfterComma < 0){
-    return 'Please choose number of Symbols after comma higher than 0';
-  }
-
- let randomNumber = getRandomNumber(min,max); 
-
-  if (typeof randomNumber  === 'number') {
-    return (randomNumber).toFixed(numberOfSymbolsAfterComma);
-  }
-
-  return randomNumber;
+  return (randomNumber).toFixed(numberOfSymbolsAfterComma);
 
 }
 
-getRandomFractionalNumber(0.1,1.2,3); 
-getRandomIntegralNumber(0,100); 
-
-
-
-// Источники:
-
-// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
+getRandomNumber(1,1000,5);
+getRandomNumber(1,100);
