2ee8f6208f8b174f705060188d8089dd6d913c84
darya <dapishta@gmail.com>
2021-10-22 12:48:57 +0800

    Made a task

--- js/show-related-offer.js
+++ js/show-related-offer.js
@@ -8,11 +8,16 @@ const map = document.querySelector('#map-canvas');
 const relatedOffers = getRelatedData();
 
 function getRoomsEnding (number) {
-  if (number === 1) {
+
+  const numbers = Array.from(String(number), Number);
+  const lastNumber = numbers[numbers.length-1];
+
+  if (lastNumber === 1) {
     return 'комната';
-  } else if (number <= 4) {
+  } else if (lastNumber > 1 && lastNumber <= 4) {
     return 'комнаты';
   }
+
   return 'комнат';
 }
 
@@ -33,12 +38,16 @@ function getRelatedOffer (item) {
   const offerDescription = newRelatedOffer.querySelector('.popup__description');
   const offerPhotos = newRelatedOffer.querySelector('.popup__photos');
   const offerAuthorAvatar = newRelatedOffer.querySelector('.popup__avatar');
+  const roomsNumber = item.offer.rooms;
+  const roomsNumberEnding = getRoomsEnding(roomsNumber);
+  const guestsNumber = item.offer.guests;
+  const guestNumberEnding = getGuestEnding(guestsNumber);
 
   offerTitle.textContent = item.offer.title;
   offerAddress.textContent = item.offer.address;
   offerPrice.textContent = `${  item.offer.price  } ₽/ночь`;
   offerType.textContent = Object.values(item.offer.type);
-  offerCapacity.textContent = `${  item.offer.rooms  } ${  getRoomsEnding(item.offer.rooms)  } для ${  item.offer.guests  } ${  getGuestEnding(item.offer.guests)  }`;
+  offerCapacity.textContent = `${  roomsNumber  } ${  roomsNumberEnding  } для ${  guestsNumber  } ${  guestNumberEnding  }`;
   offerCheckInOut.textContent = `${  item.offer.checkin  }, выезд до ${  item.offer.checkout  }`;
   offerFeatures.textContent = item.offer.features;
   item.offer.description ? offerDescription.classList.add('hidden') : offerDescription.textContent = item.offer.description;
